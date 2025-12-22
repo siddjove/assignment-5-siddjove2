d64ec56ef3247f7a81591e465a611d60be098ab9
Sidd Jove <siddjove@gmail.com>
2025-12-23 00:30:22 +0530

    CI fix: fetch aesd-assignments via HTTPS

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -10,7 +10,7 @@ AESD_ASSIGNMENTS_VERSION = 3721d62
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:siddjove/assignments-3-and-later-siddjove2.git
+AESD_ASSIGNMENTS_SITE = https://github.com/siddjove/assignments-3-and-later-siddjove2.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
