f2c18aff54b8e28be4850a1077f2bbae05b6088c
Sidd Jove <siddjove@gmail.com>
2025-12-23 01:20:56 +0530

    Update aesd-assignments version

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 4c721aa 
+AESD_ASSIGNMENTS_VERSION = 52e4fc4 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
